82e96d03174dfc9a7ab75b6bd718ce9f669c9a25
Quentin-Desmettre <quentin.desmettre@epitech.eu>
2022-02-18 11:14:35 +0100

    [+] Class definition for layer

--- Network.hpp
+++ Network.hpp
@@ -36,6 +36,31 @@ namespace deep {
     static double (*ACTIVATORS[])(double const) = {&sigmoid, &relu, &swish, &tanh, &relu6, &hardswish};
     static double (*DX_ACTIVATORS[])(double const) = {&dx_sigmoid, &dx_relu, &dx_swish, &dx_tanh, &dx_relu6, &dx_hardswish};
 
+    class Layer
+    {
+    private:
+        double (*_activator)(double const);
+        double (*_dx_activator)(double const);
+        cppm::Matrix<double> _values;
+        cppm::Matrix<double> _activated;
+
+    public:
+        Layer(cppm::uint64 const& size, Activator activator = SIGMOID);
+        Layer(cppm::Matrix<double> const& other, Activator activator = SIGMOID);
+        Layer(cppm::uint64 const& size,
+              double (*activator)(double const), double (*dx_activator)(double const));
+
+        void setActivator(Activator activator);
+        void setActivator(double (*activator)(double const), double (*dx_activator)(double const));
+        double (*getActivator(void))(double const);
+        double (*get_DxActivator(void))(double const);
+
+        const cppm::Matrix<double>& get_dxActivated(void) const;
+        const cppm::Matrix<double>& get_Activated(void) const;
+
+        void set_dxActivated(const cppm::Matrix<double>& other);
+        void set_Activated(const cppm::Matrix<double>& other);
+    };
 };
 
 #endif // AA1E3F5A_8263_4B41_8A6D_9B1C7B5EBE91
